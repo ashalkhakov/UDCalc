63cf235264c7fbf72df48d74111a68a61eed9d14
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-02-20 19:33:14 +0500

    Remove unnecessary header import.

--- Calculator/UDCalcViewController.m
+++ Calculator/UDCalcViewController.m
@@ -7,9 +7,6 @@
 
 
 #import "UDCalcViewController.h"
-#if __has_include(<QuartzCore/QuartzCore.h>)
-#import <QuartzCore/QuartzCore.h>
-#endif
 #import "UDCalcButton.h"
 #import "UDValueFormatter.h"
 #import "UDSettingsManager.h"
